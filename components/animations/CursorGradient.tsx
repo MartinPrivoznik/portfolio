085cab2fe199d1235afafa34a0ee35c3df154566
Martin Přívozník <marprivoznik@gmail.com>
2024-02-07 02:41:51 +0100

    Update CursorGradient.tsx

--- components/animations/CursorGradient.tsx
+++ components/animations/CursorGradient.tsx
@@ -1,22 +1,37 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export const CursorGradient = () => {
   const [x, setX] = useState<number>();
   const [y, setY] = useState<number>();
+  const [lastMouseYOffset, _setLastMouseYOffset] = useState<number>();
+
+  const lastMouseYOffsetRef = useRef(lastMouseYOffset);
+  const setLastMouseYOffset = (n: number) => {
+    lastMouseYOffsetRef.current = n;
+    _setLastMouseYOffset(n);
+  };
 
   useEffect(() => {
     const updateMousePosition = (ev: MouseEvent) => {
       const { clientX, clientY } = ev;
       setX(clientX);
       setY(clientY + window.scrollY);
+      setLastMouseYOffset(clientY);
+    };
+
+    const handleScroll = (e: Event) => {
+      if (lastMouseYOffsetRef.current)
+        setY(lastMouseYOffsetRef.current + window.scrollY);
     };
 
     window.addEventListener("mousemove", updateMousePosition);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
       window.removeEventListener("mousemove", updateMousePosition);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -31,7 +46,7 @@ export const CursorGradient = () => {
           );
         }
       `}</style>
-      <div className="fixed pointer-events-none opacity-5 inset-0 z-50 lg:absolute transition duration-300 backdrop-div"></div>
+      <div className="fixed pointer-events-none opacity-10 inset-0 z-50 lg:absolute transition duration-300 backdrop-div"></div>
     </>
   );
 };
