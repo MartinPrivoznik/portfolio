300ed040b9eb952ea356bee78b8def6ef2056167
Martin Přívozník <marprivoznik@gmail.com>
2024-03-07 14:13:26 +0100

    Update layout.tsx

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -26,10 +26,6 @@ export const metadata: Metadata = {
     "Martin Přívozník, Portfolio, Developer, Software engineer, Personal web",
 };
 
-export async function generateStaticParams() {
-  return siteConfig.siteLocales.map((lng) => ({ lng }));
-}
-
 export default function RootLayout({
   children,
   params,
