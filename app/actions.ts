009848f12909527cc96a6449f17839a0f0e936ec
Martin Přívozník <marprivoznik@gmail.com>
2024-06-01 01:42:44 +0200

    added own SMTP provider

--- app/actions.ts
+++ app/actions.ts
@@ -1,22 +1,38 @@
 "use server";
+import nodemailer from "nodemailer";
 
 import { ContactFormType } from "@/types";
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+import Mail from "nodemailer/lib/mailer";
 
 export async function sendContactEmail(
   formData: ContactFormType
 ): Promise<boolean> {
-  const message = {
-    to: "marprivoznik@gmail.com",
-    from: "marprivoznik@gmail.com",
+  const smtpConfig: SMTPTransport.Options = {
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: true,
+    auth: {
+      user: process.env.SMTP_AUTH_USER,
+      pass: process.env.SMTP_AUTH_SECRET,
+    },
+  };
+
+  const transport = nodemailer.createTransport(smtpConfig);
+  const transportVerified = await transport.verify();
+
+  if (!transportVerified) return false;
+
+  const message: Mail.Options = {
+    to: process.env.CONTACT_EMAIL,
+    from: process.env.SMTP_AUTH_USER,
     subject: "Message from " + formData.fullName,
-    text: `${formData.fullName} (${formData.email}) sent you a message from portfolio: ${formData.text}`,
+    text: `${formData.fullName} (${formData.email}) sent you a message from portfolio:\n${formData.text}`,
   };
 
   try {
-    const res = await sgMail.send(message);
-    if (res) return true;
+    const res = await transport.sendMail(message);
+    if (res.accepted) return true;
   } catch {
     return false;
   }
