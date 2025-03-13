d9f255af085a3e30fc796a45a37895ff64b0ad21
Martin Přívozník <marprivoznik@gmail.com>
2025-03-13 18:50:19 +0100

    fix: split chunks

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks.maxSize = 60000;
+    }
+    return config;
+  },
+};
 
 module.exports = nextConfig;
