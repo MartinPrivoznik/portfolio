e1d5f390550ee078e60f2f162b38612e2ada332e
Martin Přívozník <marprivoznik@gmail.com>
2025-03-23 16:29:38 +0100

    fix: url builder

--- helpers/UrlBuilder.ts
+++ helpers/UrlBuilder.ts
@@ -1,10 +1,10 @@
 import { siteConfig } from "@/config/site";
 
 export function buildUrl(lang: string, path: string) {
-  path = path === "/" ? "" : path;
-
   if (lang === siteConfig.defaultLocales) {
     return path;
   }
+
+  path = path === "/" ? "" : path;
   return `/${lang}${path}`;
 }
