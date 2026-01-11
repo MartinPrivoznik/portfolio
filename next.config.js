a6c4f99f661781a429be5cecac8ed3e38a97f762
Martin Přívozník <marprivoznik@gmail.com>
2026-01-11 16:22:20 +0100

    chore: build errors, optimizations

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compress: true,
+  reactStrictMode: true,
+
+  experimental: {
+    optimizePackageImports: ["@nextui-org/react"],
+  },
+};
 
 module.exports = nextConfig;
